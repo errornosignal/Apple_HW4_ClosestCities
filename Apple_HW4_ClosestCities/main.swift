8dcb5546b5389811e1749a970d7372751765f13e
errornosignal <errornosignal@users.noreply.github.com>
2017-10-03 09:17:29 -0500

    Initial Commit

---
+++ Apple_HW4_ClosestCities/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Apple_HW4_ClosestCities
+//
+//  Created by Reid Nolan on 10/3/17.
+//  Copyright © 2017 Reid Nolan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
